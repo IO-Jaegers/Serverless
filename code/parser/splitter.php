c3f0ce59bfa7bf5e80575127f092b21048e3e946
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-07-13 21:20:13 +0200

    getting a weird bug

--- code/parser/splitter.php
+++ code/parser/splitter.php
@@ -1,20 +1,23 @@
 <?php
-    mb_internal_encoding( 'UTF-8' );
-
     /**
      * Takes the input data and makes a word list of it
      */
+    mb_internal_encoding( 'UTF-8' );
+
 
     /**
      * 
      */
     function outputResult( array $values ): string
     {
-        
-        
-        
-        
-        return 'test';
+        return utf8_encode( 
+            json_encode( $values ) 
+        );
+    }
+
+    function illegal_char( string $str ): bool
+    {
+        return str_contains( $str, '[' )|| str_contains( $str, ']' ) || str_contains( $str, '#' );
     }
 
 
@@ -28,26 +31,121 @@
         $exploded = mb_split( ' ', $set );
 
         $idx = 0;
+        $size_of = count( $exploded );
 
+        // 
         for( $idx = 0; 
-             $idx < count( $exploded ); 
+             $idx < $size_of; 
              $idx++ )
         {
-            $exploded[$idx] = mb_strtolower( $exploded[ $idx ] );
+            $current = $exploded[$idx];
+
+            $illegal_char = illegal_char( $current );
+
+            if( !$illegal_char )
+            {
+                $current = mb_strtolower( $current );
+                $current = str_replace( array( "\r", "\n", "\"", "-", ".", ",", "*", ":", "_", ";" ), ' ', $current );
+
+                array_push( $retValues, $current );
+            }
         }
 
         return $retValues;
+    }
+
+    function final_split( array $values ): array
+    {
+        $ret = [];
+
+        $size_of = count( $values );
+        $idx = 0;
+
+        for( $idx = 0; 
+             $idx < $size_of; 
+             $idx++ )
+        {
+            $current = $values[$idx];
+
+            $explode = mb_split( ' ', $current );
+            $size_of_ex = count( $explode );
+
+            for( $idxEx = 0; 
+                 $idxEx < $size_of_ex; 
+                 $idxEx++ )
+            {
+                array_push( $ret, $explode[ $idxEx ] );
+            }
+        }
+
+        return $ret;
     }
 
 
     /**
      * 
      */
-    function removeRedundancies( array $values ): array
+    function removeWordsBelowThresshold( array $values ): array
+    {
+        $ret = [];
+
+        $size_of = count( $values );
+        $idx = 0;
+
+        for( $idx = 0; 
+             $idx < $size_of; 
+             $idx++ )
+        {
+            $current = $values[$idx];
+            $str_length = mb_strlen( $current );
+
+            if( $str_length >= 20 )
+            {
+                array_push( $ret, $current );
+            }
+        }
+
+        return $ret;
+    }
+
+
+    /**
+     * 
+     */
+    function removeRedundanciesAndOther( array $values ): array
     {
         $retValues = [];
 
+        $arr_size = sizeof( $values );
+
+
+        for( $index = 0;
+             $index < $arr_size;
+             $index++ )
+        {
+            $current = $values[$index];
+            $remove = false;
+
+            if( empty( $current ) )
+            {
+                $remove = true;
+            }
+
+            // 
+            if( !$remove )
+            {
+                array_push( $retValues, $current );
+            }
+        }
+
         return $retValues;
+    }
+
+    function orderWordSet( array $values ): array
+    {
+        $sV = $values;
+        $n = sort($sV, SORT_STRING);
+        return $sV;
     }
 
 
@@ -58,16 +156,22 @@
     {
         if( $arguments[ '__ow_method' ] == 'post' )
         {
-            $body = $arguments[ '__ow_body' ];
+            $body = utf8_decode( $arguments[ '__ow_body' ] );
 
             $tokens = split( $body );
-            unset( $body );
 
-            $words = removeRedundancies( $tokens );
+            $filtered_tokens = removeWordsBelowThresshold( $tokens );
             unset( $tokens );
 
+            $filtered_tokens = final_split( $filtered_tokens );
+
+            $words = removeRedundanciesAndOther( $filtered_tokens );
+            unset( $filtered_tokens );
+
             // Output value
-            return [ "body" => outputResult( $words ) ];
+            return [ "body" => outputResult( 
+                orderWordSet( $words ) 
+            ) ];
         }
         
         throw new Exception( 'wrong http type used' );
